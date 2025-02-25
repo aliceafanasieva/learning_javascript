7450092aaf1f4b5e6f88e2bab2b369253a0ab004
Alisa <alice.afanasieva@gmail.com>
2025-02-25 22:24:15 +0100

    Lesson 1: naming
    learning about naming conventions like myCurrentJob, myFirstJob, firstName etc.

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-console.log("Test message 1");
-let name = "Alisa";
-console.log("Test message 2: ", name);
-if (name === "Alisa") console.log (name, " is learning Javascript");
+let firstName = "Alisa";
+if (firstName === "Alisa") console.log (firstName, " is learning Javascript");
