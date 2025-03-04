352c25fb44e4d78b7edd1319cd06b09a90c7004b
Alisa <alice.afanasieva@gmail.com>
2025-03-04 10:49:56 +0100

    lesson if/else statements

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-let firstName = "Alisa";
-if (firstName === "Alisa") console.log (firstName, " is learning Javascript");
+// assignments
+let country = "Ukraine";
+let continent = "Europe"
+let language = "ukrainian"
+let population = 10;
+console.log(population > 6);
+console.log(population < 33);
+const description1 =
+  country +
+  ' is in ' +
+  continent +
+  ', and its ' +
+  population +
+  ' million people speak ' +
+  language;
+
+console.log(description1)
+
+const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+console.log(description2)
+
+if (population > 33) {
+  console.log(country + ' population is ' +
+  (population - 33) + ' million above average');
+} else {
+  console.log(
+    `${country}'s population is ${33 - population} million below average`
+  );
+}
